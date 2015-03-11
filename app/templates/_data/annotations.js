f497b2b260c4ca4c194707497d15412a9ce4b488
Richard Doe <richard.doe@rwdit.net>
2015-03-11 17:09:49 +0100

    Merge template changes from europeana/Europeana-Patternlab

---
+++ app/templates/_data/annotations.js
@@ -0,0 +1,29 @@
+var comments = {
+"comments" : [
+	{
+		"el": ".logo",
+	 	"title" : "Logo",
+	 	"comment": "The logo image is an SVG file, which ensures that the logo displays crisply even on high resolution displays. A PNG fallback is provided for browsers that don't support SVG images.</p><p>Further reading: <a href=\"http://bradfrostweb.com/blog/mobile/hi-res-optimization/\">Optimizing Web Experiences for High Resolution Screens</a></p>"
+	},
+	{
+		"el": ".site-hero",
+	 	"title" : "Site Hero",
+	 	"comment": "<p>The Site Hero is the main site home page hero area. It contains admin user configureable design elements - the circles position, colour and opacity, the background image, the text content, the image attribution and license</p><p>Options for selectable styling are classes that need to be added to the main outer site-hero container:</p><p><h2>Brand circles opacity:</h2> <ul> <li>default (no class): 50%</li> <li>brand-opacity25 = 25%</li> <li>brand-opacity50 = 50%</li> <li>brand-opacity75 = 75%</li> <li>brand-opacity100 = 100%</li> </ul> <h2>Brand circles position/orientation:</h2> <ul> <li>default (no class) = top left</li> <li>brand-topleft = top left</li> <li>brand-topright = top right/li> <li>brand-bottomleft = bottom left</li> <li>brand-bottomright = bottom right</li> </ul> <h2>Brand Circles colour:</h2> <ul><li>default (no class) = site accent colour</li><li>brand-colour-site = site accent colour</li><li>brand-colour-white = white</li><li>brand-colour-black = black</li></ul> </p>"
+	 },
+	{
+		"el": ".footer .nav",
+	 	"title" : "Footer Nav",
+	 	"comment": "<p>Repetition of elements isn't a bad thing, especially with potentially long scrolling pages on mobile. Providing access to the main site navigation is a good way for the user to jump off to another section and avoids leaving them with a dead end. Also, some mobile sites like <a href=\"http://bagcheck.com/\">Bagcheck</a> and <a href=\"http://contentsmagazine.com/\">Contents Magazine</a> keep the primary navigation at the footer and simply link to it with an anchor in the header. That way the nav stays accessible but the focus stays on on the core page content.</p>"
+	},
+	{
+		"el": ".tel",
+	 	"title" : "Customer Service Number",
+	 	"comment": "<p>We sometimes forget that <a href=\"http://bradfrostweb.com/blog/mobile/a-tel-tale-sign/\">mobile phones can make phone calls</a>. Whether a user is having trouble with the site or simply has some questions about the product he's about to buy, it's a smart decision to provide a clickable phone number to facilitate that call. What happens when desktops and other non-phone devices click on the <a href=\"http://www.mobilexweb.com/blog/click-to-call-links-mobile-browsers\">tel link</a>? Well, some devices (like iPads and other tablets) ask the user if they'd like to add the number to their contact list, other desktops open 3rd party VoIP programs like Skype, and others simply give an error message.</p>"
+	},
+	{
+		"el": ".top",
+	 	"title" : "Back to Top Link",
+	 	"comment": "<p>Back to top links are simple yet underrated. They provide users with an easy way back up to the top of the page with minimum effort. This is especially helpful on mobile devices, which tend to have long scrolling pages.</p><p>We're using an HTML character for the back to top arrow in order to reduce image elements and keep things looking crisp on high res displays.</p>"
+	}
+]
+};
