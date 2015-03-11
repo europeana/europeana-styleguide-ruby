0513f360d84eb33b444d25db78f099d2a4e2d3c8
Richard Doe <richard.doe@rwdit.net>
2015-03-11 17:05:26 +0000

    Add app/templates to engine's app/views paths, instead of hardcoding to gem's.

--- config/initializers/stache.rb
+++ config/initializers/stache.rb
@@ -1,7 +1,5 @@
 require 'stache'
 
 Stache.configure do |c|
-  c.template_base_path = File.expand_path('../../../app/templates', __FILE__)
-
   c.use :mustache
 end
--- lib/europeana/patternlab/engine.rb
+++ lib/europeana/patternlab/engine.rb
@@ -2,6 +2,7 @@ module Europeana
   module Patternlab
     class Engine < Rails::Engine
       engine_name 'europeana_patternlab'
+      paths['app/views'] << 'app/templates'
     end
   end
 end
