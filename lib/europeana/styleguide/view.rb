72bb19274b34949009464b5d69cff327d855cca3
Richard Doe <richard.doe@rwdit.net>
2016-09-28 08:57:11 +0100

    Add meta referrer tag

--- lib/europeana/styleguide/view.rb
+++ lib/europeana/styleguide/view.rb
@@ -23,7 +23,8 @@ module Europeana
           { meta_name: 'HandheldFriendly', content: 'True' },
           { httpequiv: 'Content-Type', content: 'text/html; charset=utf-8' },
           { meta_name: 'csrf-param', content: 'authenticity_token' },
-          { meta_name: 'csrf-token', content: form_authenticity_token }
+          { meta_name: 'csrf-token', content: form_authenticity_token },
+          { meta_name: 'referrer', content: 'always' }
         ]
       end
 
